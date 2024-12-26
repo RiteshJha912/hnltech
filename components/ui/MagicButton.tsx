43ed6cb705ccf3de1d2928db5692d2185824a235
RiteshJha912 <141942049+RiteshJha912@users.noreply.github.com>
2024-12-27 00:37:56 +0530

    magic button gradient fix

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -18,7 +18,7 @@ const MagicButton = ({
       className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
       onClick={handleClick}
     >
-      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#8F00FF_0%,#00BFFF_50%,#8F00FF_100%)]" />
+      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#00BFFF_0%,#8F00FF_50%,#00BFFF_100%)]" />
 
       <span
         className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
