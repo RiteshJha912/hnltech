36a7d8817a6ace40c2d18d7986a12554cf7c289a
RiteshJha912 <141942049+RiteshJha912@users.noreply.github.com>
2024-12-27 21:25:16 +0530

    Footer almost

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,6 @@ import MagicButton from './ui/MagicButton'
 import { FaLocationArrow } from 'react-icons/fa6'
 import { socialMedia } from '@/data'
 
-
 const Footer = () => {
   return (
     <footer className='w-full pt-20 pb-10' id='contact'>
@@ -12,7 +11,7 @@ const Footer = () => {
           Ready to take <span className='text-blue'>your</span> business to the
           next level ?
         </h1>
-        <p className='text-white-200 md:mt-10 my-5 text-center'>
+        <p className='text-white-200 md:mt-10 mt-6 sm:mt-8 my-5 text-center'>
           Reach out to us today to explore how our tailored technology solutions
           can empower your business and drive growth.
         </p>
@@ -21,12 +20,13 @@ const Footer = () => {
             title='lets get in touch'
             icon={<FaLocationArrow />}
             position='right'
+            className="mt-6 sm:mt-8" 
           />
         </a>
       </div>
 
       <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
-        <p className='md:text-base text-sm md:font-normal font-light mt-5'>
+        <p className='md:text-base text-sm md:font-normal font-light mt-5 sm:mt-8'>
           Copyright © 2024 HNLTech Pvt. Ltd.
         </p>
 
@@ -34,7 +34,7 @@ const Footer = () => {
           {socialMedia.map((profile) => (
             <div
               key={profile.id}
-              className='w-10 h-10 cursor-pointer flex justify items-center backdrop-blue-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 mt-5 mb-10'
+              className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-blue-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 mt-5 sm:mt-8 mb-10'
             >
               <img src={profile.img} alt={profile.id} width={20} height={20} />
             </div>
